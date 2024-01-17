5c8e5ec2050c9e08a561082bb74a9743f546a8e6
Kukri-2065 <154060551+Kukri-2065@users.noreply.github.com>
2024-01-17 12:57:08 +0530

    add and sub funcs added

--- calculate.py
+++ calculate.py
@@ -1,6 +1,29 @@
-
 class calculator:
   def __init__(self,a,b):
       self.a = a
       self.b = b
-  
+  def add(self):
+      return self.a+self.b
+
+  def sub(self):
+      return self.a-self.b
+
+
+while(1):
+    print("Enter option for calculator :")
+    print("1 to Add ,2 to sub , 0 to close")
+
+    choice = int(input("enter your choice : "))
+
+    if choice == 1:
+        a = int(input("enter num 1 to add : ",))
+        b = int(input("enter num 2 to add : ",))
+        cal = calculator(a, b)
+        print("Total sum for {0} and {1} is {2}".format(a,b,cal.add()))
+    elif choice == 2:
+        a = int(input("enter num 1 to add : ", ))
+        b = int(input("enter num 2 to add : ", ))
+        cal = calculator(a,b)
+        print("Substraction for {0} and {1} is {2}".format(a,b,cal.sub()))
+    else:
+        break
